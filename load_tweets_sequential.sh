959920e8bc3108a79b3eff0ce2cb681719a98c7a
Nick Wilson <Nicholas.Wilson.23@cmc.edu>
2023-04-11 23:22:46 -0700

    port number

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -17,4 +17,4 @@ time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:30360/ 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:53990/ --inputs $files
+time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:53991/ --inputs $files
